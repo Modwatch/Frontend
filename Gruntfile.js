d8c8fdcf8cf31589ddddad7d085746dad53ee8a0
ansballard <ans.ballard@gmail.com>
2015-05-26 23:14:53 -0500

    renamed stylesheets, improved grunt to be a real build system, grunt to build, grunt serve to serve, need to add concat of bower deps

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,46 +1,40 @@
 module.exports = function(grunt) {
 
   grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
+    pkg: grunt.file.readJSON("package.json"),
     uglify: {
-      backend: {
+      node: {
         options: {
-          banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n',
+          banner: "/*! <%= pkg.name %> <%= grunt.template.today(\"dd-mm-yyyy\") %> */\n",
           sourceMap: true
         },
         files: [
           {
             expand: true,
-            cwd: 'src/root/',
-            src: ['*.js'],
-            dest: './',
-            ext: '.min.js'
+            cwd: "src/root/",
+            src: ["*.js"],
+            dest: "./",
+            ext: ".min.js"
           }
         ]
       },
       angular: {
         options: {
-          banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+          banner: "/*! <%= pkg.name %> <%= grunt.template.today(\"dd-mm-yyyy\") %> */\n"
         },
         files: [
           {
-            src: 'tmp/angular/app.concat.js',
-            dest: 'public/angular/app.min.js'
+            src: "tmp/angular/annotated.ng.js",
+            dest: "public/angular/ng.js"
           }
         ]
       }
     },
     cssmin: {
-      target: {
-        files: [
-          {
-            expand: true,
-            cwd: 'src/stylesheets/',
-            src: ['*.css'],
-            dest: 'public/stylesheets/',
-            ext: '.min.css'
-          }
-        ]
+      dist: {
+        files: {
+          "public/css/all.css": "src/css/*.css"
+        }
       }
     },
     imagemin: {
@@ -49,10 +43,10 @@ module.exports = function(grunt) {
           optimizationLevel: 3
         },
         files: {
-          'public/images/FNV_NEXUS_small.png': 'src/images/FNV_NEXUS_small.png',
-          'public/images/congruent_outline.png': 'src/images/congruent_outline.png',
-          'public/images/GitHub-Logo_invert.png': 'src/images/GitHub-Logo_invert.png',
-          'public/images/ajax-loader.gif': 'src/images/ajax-loader.gif'
+          "public/images/FNV_NEXUS_small.png": "src/images/FNV_NEXUS_small.png",
+          "public/images/congruent_outline.png": "src/images/congruent_outline.png",
+          "public/images/GitHub-Logo_invert.png": "src/images/GitHub-Logo_invert.png",
+          "public/images/ajax-loader.gif": "src/images/ajax-loader.gif"
         }
       }
     },
@@ -62,56 +56,94 @@ module.exports = function(grunt) {
         add: true
       },
       dist: {
-        files: [
-          {
-            expand: true,
-            cwd: 'src/angular/',
-            src: ['*.js'],
-            dest: 'tmp/angular/',
-            ext: '.annotated.js'
-          }
+        files: {
+          "tmp/angular/annotated.ng.js": ["src/angular/app.js", "src/angular/*.js"]
+        }
+      }
+    },
+    clean: {
+      tmp: ["tmp"],
+      angular: ["tmp/angular"]
+    },
+    wiredep: {
+      dist: {
+        options: {
+          ignorePath: "\.\./public"
+        },
+        src: [
+          "views/home.html"
         ]
       }
     },
-    concat: {
+    injector: {
+      dist: {
+        files: {
+          "views/home.html": ["public/angular/ng.js", "public/css/all.css"]
+        },
+        options: {
+          transform: function(path, index, length) {
+            if(length > 0 && path !== undefined) {
+              if(path.indexOf(".css") > 0) {
+                return "<link rel=\"stylesheet\" type=\"text/css\" href=\"" + path.replace("/public/", "/") + "\"/>";
+              } else {
+                return "<script type=\"text/javascript\" src=\"" + path.replace("/public/", "/") + "\"></script>";
+              }
+            } else {
+              return "";
+            }
+          }
+        }
+      }
+    },
+    cacheBust: {
       options: {
-        separator: ';',
-        stripBanners: true
+        encoding: "utf8",
+        algorithm: "md5",
+        length: "16",
+        deleteOriginals: false,
+        rename: false,
+        baseDir: "public/"
       },
       dist: {
-        files: [
-          {
-            src: ['tmp/angular/app.annotated.js','tmp/angular/*.annotated.js'],
-            dest: 'tmp/angular/app.concat.js'
-          }
-        ]
+        files: [{
+          src: "views/home.html"
+        }]
       }
     },
-    clean: ['tmp'],
+    concurrent: {
+      inject: ["wiredep:dist", "injector:dist"],
+      minify: ["cssmin:dist", "imagemin:dist", "ngMin", "uglify:node"],
+      serve: {
+        options: {
+          logConcurrentOutput: true
+        },
+        tasks: ["watch", "nodemon:dev"]
+      }
+    },
+    nodemon: {
+      dev: {
+        script: "app.min.js"
+      }
+    },
     watch: {
       css: {
-        files: ['src/stylesheets/*.css'],
-        tasks: ['cssmin']
+        files: ["src/css/*.css"],
+        tasks: ["cssmin:dist"]
       },
       backend: {
-        files: ['src/root/*.js'],
-        tasks: ['uglify:backend']
+        files: ["src/root/*.js"],
+        tasks: ["uglify:node"]
       },
       angular: {
-        files: ['src/angular/*.js'],
-        tasks: ['ngAnnotate','concat','uglify:angular','clean']
+        files: ["src/angular/*.js"],
+        tasks: ["ngMin"]
       }
     }
   });
 
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-cssmin');
-  grunt.loadNpmTasks('grunt-contrib-imagemin');
-  grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-ng-annotate');
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-watch');
+  require('jit-grunt')(grunt);
 
-  grunt.registerTask('default', ['ngAnnotate','concat','uglify:angular','clean','uglify:backend','cssmin']);
-  grunt.registerTask('angularMin', ['ngAnnotate','concat','uglify:angular'])
+  grunt.registerTask("default", ["clean:tmp", "concurrent:minify", "concurrent:inject", "cacheBust:dist", "clean:tmp"]);
+  grunt.registerTask("serve", ["concurrent:serve"]);
+  grunt.registerTask("ngMin", ["ngAnnotate:dist", "uglify:angular", "clean:angular"])
 };
--- src/root/routes.js
+++ src/root/routes.js
@@ -1,72 +1,14 @@
 module.exports = function(app) {
 
 	app.get('/', function(req, res) {
-		res.sendfile('views/home.html');
+		res.sendFile("home.html", {root: "views/"}, function(err) {
+			if(err) {
+				res.sendStatus(500);
+			}
+		});
 	});
 	app.get('/u/:username', function(req, res) {
 	  res.redirect("/#/u/"+req.params.username);
 	});
-	/*app.get('/users', function(req, res) {
-		res.render('allusers.ejs');
-	});
-	app.get('/:username', function(req, res) {
 
-		Modlist.findOne({username: req.params.username},{username:1}, function(err, _list) {
-			if(!_list) {
-				res.redirect('/');
-			}
-			else {
-				res.render('profile.ejs', {
-					username: _list.username,
-					owner: (req.user != undefined && req.user.username == req.params.username) ? true : false
-				});
-			}
-		});
-	});
-
-	app.post('/usersearch', function(req, res) {
-		res.redirect('/'+req.body.username);
-	});
-	app.post('/:username/newpass', function(req, res) {
-		Modlist.findOne({'username' : req.params.username}, function(err, _modlist) {
-			if(_modlist) {
-				if(_modlist.validPassword(req.body.oldPassword)) {
-					_modlist.password = _modlist.generateHash(req.body.newPassword);
-					_modlist.save(function(err) {
-						if(err) {
-							console.log(err);
-						} else {
-							// nope
-						}
-					});
-					res.statusCode = 200;
-					res.write("Password changed");
-					res.end();
-				} else {
-					res.statusCode = 403;
-					res.write("Access denied, incorrect password");
-					res.end();
-				}
-			} else {
-				res.statusCode = 400;
-				res.write("No username found");
-				res.end();
-			}
-		});
-	});
-*/
 };
-/*
-var Blog = require('./public/models/blog.min.js');
-var Modlist = require('./public/models/modlist.min.js');
-function isLoggedIn(req, res, next) {
-
-	if(req.isAuthenticated()) {
-		return next();
-	}
-	else {
-		res.writeHead(403);
-		res.end();
-	}
-}
-*/
