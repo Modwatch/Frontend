99001820ccf689a55a51b2d8166bca8515afddf3
Aaron Ballard <ans.ballard@gmail.com>
2016-10-09 19:32:15 -0500

    clear sw, it was breaking stuff

--- src/js/serviceworker.js
+++ src/js/serviceworker.js
@@ -1,3 +1,9 @@
+// if ("serviceWorker" in navigator) {
+// 	navigator.serviceWorker.register("/sw.js");
+// }
 if ("serviceWorker" in navigator) {
-	navigator.serviceWorker.register("/sw.js");
+	navigator.serviceWorker.getRegistrations().then(registrations => {
+	 for(let registration of registrations) {
+	  registration.unregister()
+	} })
 }
