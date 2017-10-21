3a78397f668205b12e60d19197b17858b702bcc4
Aaron Ballard <ans.ballard@gmail.com>
2017-10-21 08:32:06 -0500

    WIP automatic port and rearranging micro and pkg files

--- pkg.js
+++ pkg.js
@@ -1,6 +1,12 @@
 /*eslint-env node*/
 const micro = require("micro");
+const getPort = require("get-port");
+const opn = require("opn");
 const routes = require("./index.js");
 const { _ } = require("minimist")(process.argv.slice(2));
 
-micro(routes).listen(_[0] || 80);
+getPort({port: _[0] || 80})
+.then(port => {
+  micro(routes).listen(port);
+  opn(`http://local.modwat.ch${port === 80 ? "" : `:${port}`}`)
+});
