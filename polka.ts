2b0fe1164f0f91c5cd59f76eb457de09a2ca87ca
Aaron Ballard <ans.ballard@gmail.com>
2020-07-31 00:00:18 -0400

    replace micro with polka, simpler and way fewer/smaller deps

--- polka.ts
+++ polka.ts
@@ -1,26 +1,13 @@
-// import { ServerRequest, ServerResponse } from "microrouter";
-
-// import { router, get, post, del } from "microrouter";
-// import { send } from "micro";
-// import Cors from "micro-cors";
-import { encode } from "jwt-simple";
 import polka from "polka";
-// import UrlPattern from "url-pattern";
+import { encode } from "jwt-simple";
+import Cors from "cors";
 
 import * as mocks from "./src/__helpers__/mocks";
 
-// const cors = Cors();
-// const usernameRegex = {
-//   segmentValueCharset: "a-zA-Z0-9-_~ %@!\\.'\\(\\)\\[\\]"
-// }
-
-// async function throttledSend(res: ServerResponse, status: number, response?: any) {
-//   return new Promise(resolve => {
-//     setTimeout(() => resolve(send(res, status, response)), Math.random() * 50 + 175);
-//   });
-// }
-
 const app = polka();
+const cors = Cors({ origin:true });
+
+app.use(cors);
 
 interface RouteDefResponseFunction {
   (params: any): any;
@@ -34,19 +21,19 @@ type RouteDef = {
 const routes: RouteDef[] = [{
   method: "get",
   url: "/api/user/:username/file/:filetype",
-  response: params => mocks[params.filetype]
+  response: params => JSON.stringify(mocks[params.filetype])
 }, {
   method: "get",
   url: "/api/user/:username/profile",
-  response: mocks.profile
+  response: JSON.stringify(mocks.profile)
 }, {
   method: "get",
   url: "/api/user/:username/files",
-  response: mocks.files
+  response: JSON.stringify(mocks.files)
 }, {
   method: "get",
   url: "/api/user/:username/all",
-  response: mocks.all
+  response: JSON.stringify(mocks.all)
 }, {
   method: "post",
   url: "/api/user/:username/delete"
@@ -56,19 +43,20 @@ const routes: RouteDef[] = [{
 }, {
   method: "get",
   url: "/api/users/count",
-  response: mocks.users.length
+  response: JSON.stringify(mocks.users.length)
 }, {
   method: "get",
-  url: "/api/users/list(/:limit)",
-  response: mocks.users
+  url: "/api/users/list/:limit?",
+  response: JSON.stringify(mocks.users)
 }, {
   method: "get",
   url: "/api/search/users/:query/:limit",
-  response: params => mocks.users.filter(user => user.username.toLowerCase().includes(params.query.toLowerCase()))
+  response: params => JSON.stringify(mocks.users.filter(user => user.username.toLowerCase().includes(params.query.toLowerCase())))
 }];
 
 routes.forEach(({ method, url, response }) => {
   app[method](url, (req, res) => {
+    console.log(method, url, response);
     res.end(typeof response === "function" ? response(req.params) : response)
   })
 });
@@ -100,5 +88,5 @@ app.delete("/oauth/user/:username/delete", async (req, res) => {
 
 app.listen(3001, err => {
   if(err) throw err;
-  console.log("localhost:3001");
+  console.log("Local API running at http://localhost:3001...");
 });
